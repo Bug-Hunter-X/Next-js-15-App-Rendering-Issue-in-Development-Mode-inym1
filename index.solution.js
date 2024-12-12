f64e14c15f01170e3205b433544e9c10243c38df
BugHunter-X <askzenai@gmail.com>
2024-12-12 08:44:06 +0100

    Add index.solution.js file

---
+++ index.solution.js
@@ -0,0 +1,13 @@
+```javascript
+// pages/index.js
+export default function Home() {
+  // Added a console log to check if the component mounts
+  console.log('Home component mounted');
+
+  return (
+    <div>
+      <h1>Next.js 15 App</h1>
+    </div>
+  );
+}
+```
